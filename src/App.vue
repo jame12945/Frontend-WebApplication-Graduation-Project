385cc95252f448145bc2b0f469ac5bf76c315fc0
Pracharapol <kongkmite02@gmail.com>
2023-09-20 16:55:39 +0700

    apploginnavbar

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,17 @@
 </template>
 
 <style>
-#app {
+/* #app {
+  font-family: 'serif';
+  margin: 0;
+  padding: 0;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+} */
+*{
+  margin: 0;
   font-family: 'serif';
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  background-image: url(/Picture/bg.jpg);
-  background-position: center;
-  background-size: cover;
-
 }
 </style>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="login-container">
-    <h1 style="text-align: center;">Login</h1>
+    <h1 style="text-align: center; font-size: 20px; margin-bottom: 25px; margin-top: 15px;">LOGIN</h1>
     <form @submit.prevent="login">
       <div class="form-group">
         <label for="username">Username</label>
@@ -10,7 +10,8 @@
         <label for="password">Password</label>
         <input type="password" id="password" v-model="password" required>
       </div>
-      <button type="submit">Login</button>
+      <button type="submit">Submit</button>
+      <div class="click">Dont have account? <router-link to="/Register" class="here">Click here</router-link></div>
     </form>
   </div>
 </template>
@@ -45,49 +46,69 @@ export default {
 <style scoped>
 
 .login-container {
-  max-width: 400px;
+  max-width: 320px;
   margin: 0 auto;
   padding: 10px;
-  border: 2px solid #6a6a6a;
-  border-radius: 5px;
-  background-color: rgba(0, 0, 0, 0.15);
+  border-radius: 10px;
+  background-color: rgba(255, 255, 255, 0.7);
 }
 
 .form-group {
   margin-bottom: 15px;
+  
 }
-
+.click {
+  margin-bottom: 25px;
+  margin-top: 20px;
+  text-align: center;
+}
+.here{
+  text-decoration: underline;
+  color: red;
+}
+.here:hover{
+  color: blue;
+  transition: 0.5s;
+}
 label {
   display: block;
   margin-bottom: 5px;
-  
-  
+  text-align: left;
+  margin-left: 17px;
+  font-weight: bold;
 }
 
 input {
-  width: 90%;
+  width: 280px;
   height: 25px;
-  padding: 0px;
-  border: 2px solid #afafaf;
-  border-radius: 3px;
+  padding: 5px 0px;
+  border: 1.5px solid #885B3B;
+  border-radius: 5px;
   position: relative;
   padding-left: 5px;
+  background-color: rgba(255, 255, 255, 0.7);
+  color: #000000;
+  margin-left: 17px;
 }
 
 button {
-  background-color: #000000;
+  background-color: #885B3B;
   color: #fff;
-  padding: 8px 15px;
-  border: 2px solid #ffffff;
-  border-radius: 3px;
+  padding: 10px 15px;
+  border: 1.5px solid #ffffff;
+  border-radius: 5px;
+  width: 288px;
   cursor: pointer;
-  
+  font-weight: bold;
+  font-family: serif;
+  margin-left: 15px;
+  margin-top: 15px;
 }
 
 button:hover{
   background-color:#ffffff;
-  border: 2px solid #000000;
-  color: #000000;
+  border: 1.5px solid #885B3B;
+  color: #885B3B;
   transition: 0.3s;
 }
 
